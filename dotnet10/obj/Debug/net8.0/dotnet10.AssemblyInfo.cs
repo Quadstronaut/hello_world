dc250156e910070d8754d9181eaf35c7b72bdde2
gitkagehub <khgreenprime@gmail.com>
2026-03-28 22:02:39 -0700

    update

--- dotnet10/obj/Debug/net8.0/dotnet10.AssemblyInfo.cs
+++ dotnet10/obj/Debug/net8.0/dotnet10.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet10")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed272f406c84503e6fe81328adb36922d2307806")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+225a862fd774efa37b86b2c02779c184bc231965")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet10")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet10")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
